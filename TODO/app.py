c4a2395e514204310901749e96a0ef82ea8cc964
ALVF <arindhimar116@gmail.com>
2024-10-17 23:12:26 +0530

    MVC

--- TODO/app.py
+++ TODO/app.py
@@ -175,5 +175,10 @@ def dashboard():
     conn.close()
     return render_template("index.html", todos=todos)
 
+@app.route('/', methods=['GET'])
+def home():
+    return render_template('login.html')
+
+
 if __name__ == "__main__":
     app.run(debug=True)
